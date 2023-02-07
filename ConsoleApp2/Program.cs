94f3ac246aaf3b90cdde5ec2b429b816a9694e53
Ronny Porsch <45596805+ronnyporsch@users.noreply.github.com>
2023-02-07 10:55:40 +0100

    added missing class

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using ConsoleApp2;
+
+public class Program
+{
+    private static string pathToJson = @"../../../Data/frame.json";
+    
+    public static void Main()
+    {
+        string[] lines = File.ReadAllLines(pathToJson);
+        string json = String.Concat(lines);
+        FrameV2? frame = JsonSerializer.Deserialize<FrameV2>(json);
+
+        Console.WriteLine(frame?.nameOfTask);
+        Console.WriteLine(frame?.frameNumber);
+        Console.WriteLine(frame?.sensors[1].quaternion?.q0);
+    }
+}
